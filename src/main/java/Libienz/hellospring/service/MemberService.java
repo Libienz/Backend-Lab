7423defc83838d5c3c05bd0e347e00499ad11c59
Lkh-Dev <Leekh10195@gmail.com>
2022-07-14 12:33:26 +0900

    about Spring bean, container

--- src/main/java/Libienz/hellospring/service/MemberService.java
+++ src/main/java/Libienz/hellospring/service/MemberService.java
@@ -3,15 +3,19 @@ package Libienz.hellospring.service;
 import Libienz.hellospring.domain.Member;
 import Libienz.hellospring.repository.MemberRepository;
 import Libienz.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+
 
 public class MemberService {
 
     private final MemoryMemberRepository memberRepository;
 
-    public MemberService(MemberRepository memberRepository) {
+    @Autowired
+    public MemberService(MemoryMemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
     public Long join(Member member) {
