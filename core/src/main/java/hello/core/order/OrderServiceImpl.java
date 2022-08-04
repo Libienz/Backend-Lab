d3af8112fbdb67d7af481766c195ccd6b378f0e0
Lkh-Dev <Leekh10195@gmail.com>
2022-08-04 18:40:46 +0900

    Lombok dependency added

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -6,30 +6,18 @@ import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor //final이 붙은거는 required 요구되는 것임 요거에 대해서 생성자를 자동으로 만들어줌
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository;// = new MemoryMemberRepository();
-    //private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private final DiscountPolicy discountPolicy;
 
-    public MemberRepository getMemberRepository() {
-        return memberRepository;
-    }
 
-    @Autowired //생성자가 한개라면 생략해도 괜춘 요새는 생략하는 추세임
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
-    //private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
-    //수정하는 과정에서 DIP와 OCP 위반 이거를 어떻게 해결할 수 있을까?
 
-    //인터페이스만 의존하도록 설정했다. but 당연히 구현체가 없으니 널포인트익셉션 발생
-    private DiscountPolicy discountPolicy;
-    //해결하기 위해 누군가 구현 객체를 꽂아주어야 한다.
 
 
     @Override
