c949e950db42c4ec412ff4de36b52a1b5b2929a8
Kuen Hee Lee <Leekh10195@gmail.com>
2023-08-30 12:34:10 +0900

    JPA 활용2: Section 04 done

--- jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -75,7 +75,7 @@ public class OrderApiController {
 
     @GetMapping("/api/v4/orders")
     public List<OrderQueryDto> ordersV4() {
-        orderQueryRepository.findOrderQueryDtos();
+        return orderQueryRepository.findOrderQueryDtos();
     }
     @Getter
     static class OrderDto {
