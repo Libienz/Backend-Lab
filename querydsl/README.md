e23f6779735ed157c2105d350d7c7f721686c459
Kuen Hee Lee <Leekh10195@gmail.com>
2023-09-07 01:08:07 +0900

    Querydsl: Section 03 결과조회

--- querydsl/README.md
+++ querydsl/README.md
@@ -99,5 +99,99 @@ tasks.named('test') {
   - 동시성 문제는 JPAQueryFactory를 생성할 때 제공하는 EntityManager에 달려있다.
   - 스프링 프레임워크는 여러 쓰레드에서 동시에 같은 EntityManager에 접근해도, 트랜잭션 마다 별도의 영속성 컨텍스트를 제공하기 때문에, 동시성 문제는 걱정하지 않아도 된다.
 
+### 기본 Q-Type 활용
+- Qtype이란 쿼리를 빌드하기 위한 타입
+- Q클래스 인스턴스를 사용하는 2가지 방법
+
+```java
+QMember qMember = new QMember("m"); //별칭 직접 지정
+Qmember qMember = Qmember.member; //기본 인스턴스 사용
+```
+
+### 검색 조건 쿼리
+- Querydsl은 JPQL이 제공하는 모든 검색 조건을 제공한다.
+
+```java
+member.username.eq("member1") // username = 'member1'
+member.username.ne("member1") //username != 'member1'
+member.username.eq("member1").not() // username != 'member1'
+member.username.isNotNull() //이름이 is not null
+member.age.in(10, 20) // age in (10,20)
+member.age.notIn(10, 20) // age not in (10, 20)
+member.age.between(10,30) //between 10, 30
+member.age.goe(30) // age >= 30
+member.age.gt(30) // age > 30
+member.age.loe(30) // age <= 30
+member.age.lt(30) // age < 30
+member.username.like("member%") //like 검색
+member.username.contains("member") // like ‘%member%’ 검색
+member.username.startsWith("member") //like ‘member%’ 검색
+...
+```
+
+### AND 조건을 파라미터로 처리
+
+```java 
+
+    @Test
+    public void searchAndParam() throws Exception {
+        Member findMember = queryFactory
+                .select(member)
+                .from(member)
+                .where(
+                        member.username.eq("member1"),
+                        member.age.eq(10)
+                )
+                .fetchOne();
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+```
+- where()에 파라미터로 검색조건을 추가하면 AND조건이 추가됨
+- 이 경우 null 값은 무시 -> 메서드 추출을 활용해서 동적 쿼리를 깔끔하게 만들 수 있음
+
+### 결과 조회
+- fetch(): 리스트 조회, 데이터 없으면 빈 리스트 반환
+- fetchOne(): 단 건 조회
+  - 결과가 없으면 null
+  - 결과가 둘 이상이면 NonUniqueResultException
+- fetchFirst(): limit(1).fetchOne()
+- fetchResults(): 페이징 정보 포함, totalCount 쿼리 추가 실행
+- fetchCount(): count 쿼리로 변경해서 수 조회
+
+```java
+
+    @Test
+    public void resultFetch() throws Exception {
+        //리스트 조회
+        List<Member> fetch = queryFactory
+                .select(member)
+                .from(member)
+                .fetch();
+        //단건 조회
+        Member fetchOne = queryFactory
+                .selectFrom(member)
+                .fetchOne();
+        //limit(1) 조회
+        Member fetchFirst = queryFactory
+                .selectFrom(member)
+                .fetchFirst();
+
+        //fetchResults.getTotal() 제공 등등 페이징 관련 제공
+        QueryResults<Member> fetchResults = queryFactory
+                .selectFrom(member)
+                .fetchResults();
+        fetchResults.getTotal();
+        fetchResults.getLimit();
+        fetchResults.getOffset();
+
+        //countQuery
+        long total = queryFactory
+                .selectFrom(member)
+                .fetchCount();
+
+    }
+
+```
 </div>
 </details>
--- querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
+++ querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -1,5 +1,7 @@
 package study.querydsl;
 
+import com.querydsl.core.QueryResults;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import org.assertj.core.api.Assertions;
@@ -12,7 +14,10 @@ import study.querydsl.entity.Member;
 import study.querydsl.entity.QMember;
 import study.querydsl.entity.Team;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.*;
+import static study.querydsl.entity.QMember.*;
 
 @SpringBootTest
 @Transactional
@@ -20,6 +25,7 @@ public class QuerydslBasicTest {
 
     @Autowired
     EntityManager em;
+    JPAQueryFactory queryFactory;
 
 
     @BeforeEach
@@ -38,6 +44,7 @@ public class QuerydslBasicTest {
         em.persist(member2);
         em.persist(member3);
         em.persist(member4);
+        queryFactory = new JPAQueryFactory(em);
 
     }
 
@@ -48,19 +55,75 @@ public class QuerydslBasicTest {
                 .getSingleResult();
         assertThat(findMember.getUsername()).isEqualTo("member1");
      }
-     @Test
-     public void startQuerydsl() throws Exception {
 
-         JPAQueryFactory queryFactory = new JPAQueryFactory(em);
-         QMember m = new QMember("m");
+    @Test
+    public void startQuerydsl() throws Exception {
 
-         Member findMember = queryFactory
-                 .select(m)
-                 .from(m)
-                 .where(m.username.eq("member1"))
-                 .fetchOne();
+        JPAQueryFactory queryFactory = new JPAQueryFactory(em);
+//         QMember m = new QMember("m");
 
-         assertThat(findMember.getUsername()).isEqualTo("member1");
+        Member findMember = queryFactory
+                .select(member)
+                .from(member)
+                .where(member.username.eq("member1"))
+                .fetchOne();
 
-      }
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+
+    }
+
+    @Test
+    public void search() throws Exception {
+        Member findMember = queryFactory
+                .select(member)
+                .from(member)
+                .where(member.username.eq("member1")
+                        .and(member.age.eq(10)))
+                .fetchOne();
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    public void searchAndParam() throws Exception {
+        Member findMember = queryFactory
+                .select(member)
+                .from(member)
+                .where(
+                        member.username.eq("member1"),
+                        member.age.eq(10)
+                )
+                .fetchOne();
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    public void resultFetch() throws Exception {
+        //리스트 조회
+        List<Member> fetch = queryFactory
+                .select(member)
+                .from(member)
+                .fetch();
+        //단건 조회
+        Member fetchOne = queryFactory
+                .selectFrom(member)
+                .fetchOne();
+        //limit(1) 조회
+        Member fetchFirst = queryFactory
+                .selectFrom(member)
+                .fetchFirst();
+
+        //fetchResults.getTotal() 제공 등등 페이징 관련 제공
+        QueryResults<Member> fetchResults = queryFactory
+                .selectFrom(member)
+                .fetchResults();
+        fetchResults.getTotal();
+        fetchResults.getLimit();
+        fetchResults.getOffset();
+
+        //countQuery
+        long total = queryFactory
+                .selectFrom(member)
+                .fetchCount();
+
+    }
 }
