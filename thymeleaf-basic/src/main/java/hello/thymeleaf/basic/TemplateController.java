c50df24dd9cfef71dc2a72f19b1e70f343230f0e
Kuen Hee Lee <Leekh10195@gmail.com>
2023-10-01 16:11:54 +0900

    SpringMVC2: Section 01 done

--- thymeleaf-basic/src/main/java/hello/thymeleaf/basic/TemplateController.java
+++ thymeleaf-basic/src/main/java/hello/thymeleaf/basic/TemplateController.java
@@ -16,4 +16,8 @@ public class TemplateController {
     public String layout() {
         return "template/layout/layoutMain";
     }
+    @GetMapping("/layoutExtend")
+    public String layoutExtends() {
+        return "template/layoutExtend/layoutExtendMain";
+    }
 }
