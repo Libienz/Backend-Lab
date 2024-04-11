0f3ffdb776984e90bc45d388148fa8f97b79183c
libienz <leekh10195@gmail.com>
2024-04-11 19:16:59 +0900

    Spring DB 1편: section 01 JDBC 개발 - 조회

--- spring-db1/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ spring-db1/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -1,16 +1,25 @@
 package hello.jdbc.repository;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 import hello.jdbc.domain.Member;
 import java.sql.SQLException;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
+@Slf4j
 class MemberRepositoryV0Test {
 
     MemberRepositoryV0 repository = new MemberRepositoryV0();
 
     @Test
     void crud() throws SQLException {
+        //save
         Member member = new Member("memberV0", 10000);
         repository.save(member);
+        //findById
+        Member findMember = repository.findById(member.getMemberId());
+        log.info("findMember={}", findMember);
+        assertThat(findMember).isEqualTo(member);
     }
 }
